023518e58c8631fd4c47d96e280065169d1dd062
thimotypaling <thimotypaling@gmail.com>
2020-10-23 14:35:56 +0800

    First Commit

---
+++ ADETQ2_LarzThimotyPal-ing/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ADETQ2_LarzThimotyPal_ing.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
